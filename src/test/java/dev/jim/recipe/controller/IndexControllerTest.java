dd0bdaaacb6b24f4965e82d9a0adda98b8f9ac9f
james smith <smithjimdev@gmail.com>
2018-10-25 09:53:42 +0100

    Added integration test for UnitOfMeasure and unit test for index controller with simple MVC test

--- src/test/java/dev/jim/recipe/controller/IndexControllerTest.java
+++ src/test/java/dev/jim/recipe/controller/IndexControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -15,6 +17,9 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -34,6 +39,15 @@ public class IndexControllerTest {
     }
 
     @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
     public void testGetIndexPage() {
 
         //given
@@ -43,7 +57,7 @@ public class IndexControllerTest {
         recipes.add(r1);
         recipes.add(new Recipe());
 
-        
+
         when(recipeService.findAll()).thenReturn(recipes);
         ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
         String viewName = controller.getIndexPage(model);
